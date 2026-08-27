c2aa2269481739c0431f39b9669a5086d8eef53e
Alex <80811800+aalcar@users.noreply.github.com>
2026-08-27 09:19:30 -0400

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # we want our triplet to be the max
+        # get rid of everything where a > t_a or b > t_b or c > t_c
+        t_a, t_b, t_c = target
+        max_a = max_b = max_c = float('-inf')
+
+        for i in range(len(triplets)):
+            a, b, c = triplets[i]
+            if a > t_a or b > t_b or c > t_c:
+                continue
+            
+            max_a = max(max_a, a)
+            max_b = max(max_b, b)
+            max_c = max(max_c, c)
+
+        return t_a == max_a and t_b == max_b and t_c == max_c
