8095496ef5093cfe401b2d28a24d623ab689ed89
Alex <80811800+aalcar@users.noreply.github.com>
2026-08-27 12:09:02 -0400

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # track minimum and maximum amount of open left parans possible
+        leftMax = leftMin = 0
+        for c in s:
+            if c == '(':
+                leftMax += 1
+                leftMin += 1
+            elif c == ')':
+                leftMax -= 1
+                leftMin -= 1
+            else:
+                leftMax += 1
+                leftMin -= 1
+            
+            if leftMax < 0:
+                return False
+
+            if leftMin == -1:
+                leftMin = 0
+        
+        return leftMin <= 0 <= leftMax 
