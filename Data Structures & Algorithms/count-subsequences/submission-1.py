1d12a21f9441b4448a88b176762d9b2142fd327e
alex <80811800+aalcar@users.noreply.github.com>
2026-09-10 00:57:14 -0700

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,37 @@
+from functools import cache
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        """
+        subsequence:
+        pick or dont pick letters
+        has to be in original order
+        dp(i, j) -> number of distinct subsequences
+        i -> index in s str
+        j -> index in t str
+        rr:
+        dp(i, j)
+        res = dp(i + 1, j)
+        if s[i] == s[j]:
+            res += dp(i + 1, j + 1)
+        
+        base cases:
+        if i == len(s):
+            return 0
+        if j == len(t):
+            return 1
+        """
+        @cache
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+
+            if i == len(s):
+                return 0
+
+            res = dfs(i + 1, j)
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            
+            return res
+
+        return dfs(0, 0)
